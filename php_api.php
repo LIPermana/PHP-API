846b745c38fa5142c2704a164526a65829dc75d1
Lulu Irman Permana <irmanpermana.lu@gmail.com>
2018-10-19 18:29:26 +0700

    php-api

---
+++ php_api.php
@@ -0,0 +1,43 @@
+<?php
+	$koneksi = mysqli_connect("localhost","root","","php-api");
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data</title>
+</head>
+<body>
+	<table align="center" border="1" width="70%">
+		<tr>
+			<td colspan="6">
+				<h3><center>DATA</center></h3>
+			</td>
+		</tr>
+		<tr>
+			<th>ID</th>
+			<th>Username</th>
+			<th>Password</th>
+			<th>Level</th>
+			<th>Fullname</th>
+		</tr>
+			<?php
+
+				$qry = mysqli_query($koneksi,"SELECT * FROM siswa");
+				while($data = mysqli_fetch_array($qry)){
+			 ?>
+		<tr>
+			<td><?php echo $data['id']; ?></td>
+			<td><?php echo $data['username']; ?></td>
+			<td><?php echo $data['password']; ?></td>
+			<td><?php echo $data['level']; ?></td>
+			<td><?php echo $data['fullname']; ?></td>
+
+		</tr>
+
+	<?php
+}
+?>
+	</table>
+</body>
+</html>
